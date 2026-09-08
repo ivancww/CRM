ffff5316b7acdce80f53031df0316e10bc936303
ivancww <wahchiu0903@gmail.com>
2026-09-08 17:06:20 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ==========================================
 // AVA CRM & Portfolio Service Worker (v3.6.3)
 // ==========================================
-const CACHE_NAME = 'crm-cache-v6.1.0';
+const CACHE_NAME = 'crm-cache-v6.1.1';
 
 const urlsToCache = [
   '/',
